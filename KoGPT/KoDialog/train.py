ea67a0e31987759a6a8668a264b1cf3afbb5c4ea
slime0519 <leejunmyeong0519@gmai.com>
2021-07-19 01:29:34 +0900

    add argurments for training

--- KoGPT/KoDialog/train.py
+++ KoGPT/KoDialog/train.py
@@ -21,7 +21,10 @@ parser.add_argument("--batch_size", default=2, type=int)
 parser.add_argument("--save_step", default=100, type=int)
 parser.add_argument("--lr", default=5e-5)
 parser.add_argument("--save_path", type=str)
-
+parser.add_argument("--validation", default=False, type=bool)
+parser.add_argument("--train_dataset", type=str)
+parser.add_argument("--valid_dataset", type=str)
+parser.add_argument("--savename", type=str)
 train_data_path = "korean_dialog_summary/Training/label_kodialog_summary_train/personal_relationship.json"
 valid_data_path = "korean_dialog_summary/Validation/label_kodialog_summary_valid/personal_relationship.json"
 save_ckpt_path = "KoDialog_general.pth"
@@ -33,6 +36,11 @@ if __name__ =="__main__":
     save_step = args.save_step
     learning_rate = args.lr
     save_path = args.save_path
+    do_valid = args.validation
+    savename = args.savename
+    train_data_path = args.train_dataset
+    valid_data_path = args.valid_dataset
+
     tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2',
                                                         bos_token='</s>',
                                                         eos_token='</s>',
@@ -44,8 +52,9 @@ if __name__ =="__main__":
     train_batch_sampler = BucketBatchSampler(train_sampler, batch_size, True, sort_key=lambda r: len(train_dataset[r]))
     train_loader = DataLoader(train_dataset, batch_sampler=train_batch_sampler, collate_fn=collate_fn)
 
-    valid_dataset = KoDialogueDataset(tokenizer=tokenizer, datapath=valid_data_path)
-    valid_loader = DataLoader(valid_dataset, batch_size=1)
+    if do_valid:
+        valid_dataset = KoDialogueDataset(tokenizer=tokenizer, datapath=valid_data_path)
+        valid_loader = DataLoader(valid_dataset, batch_size=1)
 
     model = KoDialogGPT2()
     model.to(device)
@@ -78,7 +87,7 @@ if __name__ =="__main__":
                 losses.append(loss.item())
 
                 if (count > 0 and count % save_step == 0) or (len(data) < batch_size):
-                    save_ckpt_path = os.path.join(save_path, f"KoDialog_general_{epoch}_{count}_{np.mean(losses):.3f}.pth")
+                    save_ckpt_path = os.path.join(save_path, savename+ f"_{epoch}_{count}_{np.mean(losses):.3f}.pth")
                     torch.save({
                         'epoch': epoch,
                         'train_no': count,
@@ -90,16 +99,17 @@ if __name__ =="__main__":
                 pbar.update(1)
                 pbar.set_postfix_str(f"Loss: {loss.item():.3f} ({np.mean(losses):.3f})")
 
-            model.eval()
-            for i, data in enumerate(valid_loader):
-                data = data.to(device)
-                outputs = model(data, labels = data)
-                _, logits = outputs[:2]
+            if do_valid:
+                model.eval()
+                for i, data in enumerate(valid_loader):
+                    data = data.to(device)
+                    outputs = model(data, labels = data)
+                    _, logits = outputs[:2]
 
-                shift_logits = logits[..., :-1, :].contiguous()
-                shift_labels = data[..., 1:].contiguous()
+                    shift_logits = logits[..., :-1, :].contiguous()
+                    shift_labels = data[..., 1:].contiguous()
 
-                valid_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-                valid_losses.append(valid_loss.item())
-            print(f"Validation loss : {np.mean(valid_losses):.3f}")
+                    valid_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+                    valid_losses.append(valid_loss.item())
+                print(f"Validation loss : {np.mean(valid_losses):.3f}")
 
